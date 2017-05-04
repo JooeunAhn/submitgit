7c9ef63ea70ae538c5f4ecc51e121ad902e1aba3
JooeunAhn <jflow@hanyang.ac.kr>
2017-05-04 09:13:35 +0900

    github login view modified

--- submitgit/accounts/views.py
+++ submitgit/accounts/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter 
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from rest_auth.registration.views import SocialLoginView
@@ -8,5 +6,3 @@ from rest_auth.registration.views import SocialLoginView
 class GitHubLogin(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
     client_class = OAuth2Client
-    callback_url = "localhost:8000"
-
