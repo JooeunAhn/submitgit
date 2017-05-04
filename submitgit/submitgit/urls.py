120b3785c30f750da76a10f6e36eca06f40223c9
JooeunAhn <jflow@hanyang.ac.kr>
2017-05-04 22:10:35 +0900

    angualr added

--- submitgit/submitgit/urls.py
+++ submitgit/submitgit/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from allauth.account.views import confirm_email
 from rest_framework import routers
@@ -27,6 +28,7 @@ router.register(r'profile', ProfileViewSet)
 
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="angular/index.html"), name="index"),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.account.urls')),
     url(r'^api/v1/', include(router.urls)),
